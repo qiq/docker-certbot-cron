2b60d9c4379dced912041dbd18d185b8e295fda5
Miroslav Spousta <qiq@ucw.cz>
2017-12-07 17:05:54 +0100

    Only log warnings and errors.
    In general we are not interested in messages that cronjob was executed
    every 15 minutes.

--- renew_certificates.sh
+++ renew_certificates.sh
@@ -4,4 +4,4 @@ CRONJOB=/etc/periodic/daily/renew_certificates
 echo -e "#!/bin/sh\n/usr/local/bin/certbot -n renew $CERTBOT_ARGS --deploy-hook 'echo -e \"POST /containers/$PROXY_CONTAINER_NAME/kill?signal=SIGHUP HTTP/1.0\r\n\" |nc -U /tmp/docker.sock'" >$CRONJOB
 chmod a+x $CRONJOB
 
-exec crond -f -d 0
+exec crond -f -d 9
