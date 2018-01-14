fd10fb32db46e79c4a46f0502df388ed3bbe0548
Miroslav Spousta <qiq@ucw.cz>
2018-01-14 14:27:00 +0100

    Use standard docker socket location

--- create_certificates.sh
+++ create_certificates.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 /usr/local/bin/certbot -n -m $EMAIL_ADDRESS --agree-tos certonly $CERTBOT_ARGS --webroot --webroot-path /webroot -d $PROXY_DOMAINS
-echo -e "POST /containers/$PROXY_CONTAINER_NAME/kill?signal=SIGQUIT HTTP/1.0\r\n" |nc -U /tmp/docker.sock
+echo -e "POST /containers/$PROXY_CONTAINER_NAME/kill?signal=SIGQUIT HTTP/1.0\r\n" |nc -U /var/run/docker.sock
--- renew_certificates.sh
+++ renew_certificates.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CRONJOB=/etc/periodic/daily/renew_certificates
-echo -e "#!/bin/sh\n/usr/local/bin/certbot -n renew $CERTBOT_ARGS --deploy-hook 'echo -e \"POST /containers/$PROXY_CONTAINER_NAME/kill?signal=SIGHUP HTTP/1.0\r\n\" |nc -U /tmp/docker.sock'" >$CRONJOB
+echo -e "#!/bin/sh\n/usr/local/bin/certbot -n renew $CERTBOT_ARGS --deploy-hook 'echo -e \"POST /containers/$PROXY_CONTAINER_NAME/kill?signal=SIGHUP HTTP/1.0\r\n\" |nc -U /var/run/docker.sock'" >$CRONJOB
 chmod a+x $CRONJOB
 
 exec crond -f -d 9
